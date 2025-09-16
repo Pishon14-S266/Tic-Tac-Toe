edf40b530251a058b21f02315b0525cd3a85b970
Pishon <it25101266@my.sliit.lk>
2025-09-16 16:22:47 +0530

    Game 1 update

--- game1.c
+++ game1.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char initializeBoard(int size) {
-    int totalCells = size * size;
-    char board = malloc(totalCells);
+char *initializeBoard(int size)
+{
+    int totalBoxes = size * size;
+    char *board = malloc(totalBoxes);
 
-    if (board == NULL) {
-        printf("Memory Allocation failed\n");
-        
+    if (board == NULL)
+    {
+        printf ("Memory Allocation failed\n");
+        exit (1);
     }
 
-    for (int i = 0; i < totalCells; i++) {
-        board[i] = ' ';
-    }
+    for (int i = 0;  i < totalBoxes; i++)
+    {
+      board[i] = ' ';
 
+    }
     return board;
+    
+
+}   
+
+char *displayBoard ()
+{
+    
+}
+
+int main ()
+{
+    int size;
+    printf("Enter the size of the board: ");
+    scanf("%d", &size);
+
+    // Initializing the game board - DONE
+    intializeBoard(size);
+
+    // Dissplaying the game board
+    // Accepting user input
+    // Validating user input and moves
+    // Checking win conditions
+    // Checking for a draw
+
+    return (0);
 }

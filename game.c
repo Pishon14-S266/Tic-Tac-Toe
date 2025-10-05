5a689f1ec174ef3f4029c01262f6d10420a33b14
Pishon <it25101266@my.sliit.lk>
2025-10-05 18:19:32 +0000

    Comments

--- game.c
+++ game.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Structure for symbol and to see whether its computer or human
 typedef struct {
 	char symbol;
     	int isComputer; // 0 = human, 1 = computer
 } Player; 
 
+// To setup players (Only used when 3 player mode is being played)
 void setupPlayers(Player players[3]) 
 {
     	char symbols[3] = {'X', 'O', 'Z'};
@@ -14,7 +16,7 @@ void setupPlayers(Player players[3])
 
     	do 
 	{
-        	humanCount = 0;
+        	humanCount = 0; // To count how many humans are playing
         	for (int i = 0; i < 3; i++) 
 		{
             		players[i].symbol = symbols[i];
@@ -34,7 +36,7 @@ void setupPlayers(Player players[3])
 	while (humanCount == 0);
 }
 
-
+// Funnction to initialize the board. To allocate memory and setup the boxes
 char *initializeBoard (int size)
 {
     	int totalBoxes = size * size;
@@ -48,7 +50,7 @@ char *initializeBoard (int size)
 
     	for (int i = 0;  i < totalBoxes; i++)
     	{
-      	board[i] = ' ';
+      		board[i] = ' ';
 
     	}
     	return board;
@@ -103,12 +105,14 @@ void displayBoard (char *board, int size)
 
 }
 
+// Function to get user input during the play
 void getUserInput(int *row, int *col, char symbol)
 {
     	printf("Player %c, enter your move (row [space] column): ", symbol);
     	scanf("%d %d", row, col);
 }
 
+// Function to validate the move made
 int validateMove(char *board, int size, int row, int col)
 {
     	// Range validation
@@ -130,7 +134,7 @@ int validateMove(char *board, int size, int row, int col)
     	return index; // Valid move
 }
 
-
+// Function to check whether anyone have won
 int checkWin(char *board, int size, char symbol)
 {
     	// Check rows
@@ -199,6 +203,7 @@ int checkWin(char *board, int size, char symbol)
     	return 0;
 }
 
+// Function to check whether the game is draw
 int checkDraw(char *board, int size)
 {
     	for (int i = 0; i < size * size; i++)
@@ -212,20 +217,24 @@ int checkDraw(char *board, int size)
 }
 
 
-
+// Function to log the game data
 void logGameState(char *board, int size, char currentPlayer, int moveCount)
 {
-    	FILE *file = fopen("game_log.txt", "a"); // Open in append mode
-    	if (file == NULL)
+
+    	FILE *file = fopen("game_log.txt", "a"); // Open in append mode so that the previous logs are preserved
+    	if (file == NULL) // If the file cannot be opened, an error will be printed and exit the function
     	{
         	printf("Error opening log file.\n");
         	return;
     	}
-
+	
+	// Write move number and the symbol of the player who made the move
     	fprintf(file, "Move %d by Player %c:\n", moveCount, currentPlayer);
-
+	
+	// Loop through each row of the board
     	for (int i = 0; i < size; i++)
     	{
+		// Loop through each column in the current row
         	for (int j = 0; j < size; j++)
         	{
             		fprintf(file, " %c ", board[i * size + j]);
@@ -251,31 +260,39 @@ void logGameState(char *board, int size, char currentPlayer, int moveCount)
     	}
 
     	fprintf(file, "\n");
+
+	// Close the file to save the changes
     	fclose(file);
 }
 
+// Function to generate random but valid move when it's computer's turn
 int getComputerMove(char *board, int size) 
 {
     	int totalCells = size * size;
     	int index;
 
+	// Keep trying random positions until an empty cell is found
     	while (1) 
 	{
-        	index = rand() % totalCells;
-        	if (board[index] == ' ') 
+		// Generate a random index WHITHIN THE BOARD
+        	index = rand() % totalCells; 
+        	if (board[index] == ' ') // Checking to see if the cell is empty
         	{	
-            		return index;
+            		return index; // Return the valid move index
         	}
     	}
 }
 
+// If two player game is selected
 void playTwoPlayerGame(char *board, int size, Player players[2]) 
 {
     	int currentPlayerIndex = 0;
     	int moveCount = 0;
 
+	// Main game loop
     	while (1) 
 	{
+
         	displayBoard(board, size);
 
         	Player currentPlayer = players[currentPlayerIndex];
@@ -287,6 +304,7 @@ void playTwoPlayerGame(char *board, int size, Player players[2])
             		int row, col;
             		while (1) 
 			{
+
                 		getUserInput(&row, &col, currentPlayer.symbol);
                 		index = validateMove(board, size, row, col);
                 		if (index != -1) 
@@ -294,7 +312,7 @@ void playTwoPlayerGame(char *board, int size, Player players[2])
                     			board[index] = currentPlayer.symbol;
                     			break;
                 		} 
-				else 
+				else  
 				{
                     			printf("Invalid move. Try again.\n");
                 		}
@@ -312,6 +330,8 @@ void playTwoPlayerGame(char *board, int size, Player players[2])
         	}
 
         	moveCount++;
+
+		// Log the game to the file
         	logGameState(board, size, currentPlayer.symbol, moveCount);
 
         	if (checkWin(board, size, currentPlayer.symbol)) 
@@ -327,16 +347,19 @@ void playTwoPlayerGame(char *board, int size, Player players[2])
             		printf("It's a draw!\n");
             		break;
         	}
-
+	
+		// Switch to the next player
         	currentPlayerIndex = (currentPlayerIndex + 1) % 2;
     	}
 }
 
+// If three player game is selected
 void playThreePlayerGame(char *board, int size, Player players[3]) 
 {
     	int currentPlayerIndex = 0;
     	int moveCount = 0;
 
+	// Main game loop
     	while (1) 
 	{
         	displayBoard(board, size);
@@ -376,6 +399,8 @@ void playThreePlayerGame(char *board, int size, Player players[3])
         	}
 
         	moveCount++;
+
+		// Log the game to the file
         	logGameState(board, size, currentPlayer.symbol, moveCount);
 
         	if (checkWin(board, size, currentPlayer.symbol)) 
@@ -391,7 +416,8 @@ void playThreePlayerGame(char *board, int size, Player players[3])
             		printf("It's a draw!\n");
             		break;
         	}
-
+		
+		// Move to the next player in cyclic order
         	currentPlayerIndex = (currentPlayerIndex + 1) % 3;
     	}
 }
@@ -399,9 +425,12 @@ void playThreePlayerGame(char *board, int size, Player players[3])
 
 int main()
 {
+	// Seed the random number generator for computer moves
     	srand(time(NULL));
 
     	int size;
+
+	// Prompt user for board size
     	printf("Enter the size of the board (3-10): ");
     	scanf("%d", &size);
 
@@ -413,6 +442,8 @@ int main()
     	}
 
     	int mode;
+
+	// Prompt the user for a game mode
     	printf("\nSelect Game Mode:\n");
     	printf("1. Two Players (User vs User)\n");
     	printf("2. User vs Computer\n");
@@ -428,9 +459,11 @@ int main()
 
     	// Initialize board
     	char *board = initializeBoard(size);
-
+	
+	// Handle game mode sleection
     	if (mode == 1) 
     	{
+		// Two human players
         	Player players[2] = 
         	{
             		{'X', 0},
@@ -440,6 +473,7 @@ int main()
     	}
     	else if (mode == 2) 
     	{
+		// One human, One computer
         	Player players[2] = 
         	{
             	{'X', 0},
@@ -449,12 +483,13 @@ int main()
     	}
     	else if (mode == 3)
     	{
+		// Three player mode. Can configure roles
         	Player players[3];
-        	setupPlayers(players);
+        	setupPlayers(players); // Ask user to setup the players
         	playThreePlayerGame(board, size, players);
     	}	       
 
-    	// Free memory
+    	// Free dynamically allocated memory for the board
     	free(board);
-    	return 0;
+    	return 0; 
 }
